87dcf7bfed6c781da437c512e3fc011855b91a5e
Kayo.io <kayorenatocontato@gmail.com>
2023-02-23 10:28:29 -0300

    chore: change a to button tag in Sidebar

--- src/components/SidebarLogin.tsx
+++ src/components/SidebarLogin.tsx
@@ -1,8 +1,13 @@
 import styles from './SidebarLogin.module.css'
 import { User } from 'phosphor-react'
 
+interface SidebarProps {
+    handleEditProfile: () => void;
 
-export function SidebarLogin() {
+}
+
+
+export function SidebarLogin(props: SidebarProps) {
     return (
         <aside className={styles.sidebarLogin}>
             <div className={styles.cover}>
@@ -10,9 +15,9 @@ export function SidebarLogin() {
                     src="https://images.unsplash.com/photo-1676458482060-744386b5c273?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60" />
             </div>
             <footer>
-                <a href="#">
+                <button onClick={props.handleEditProfile}>
                     <User size={20} /> Login
-                </a>
+                </button>
             </footer>
         </aside>
     );
